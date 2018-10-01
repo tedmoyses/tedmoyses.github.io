91ca94ce12a2cae081e2bf5eafa7490ac02a16e8
Ted Moyses <tedmoyses@googlemail.com>
2018-10-01 21:59:06 +0100

    adding source files

---
+++ templates/history.blade.php
@@ -0,0 +1,33 @@
+
+@extends('layout')
+@section('title', 'History')
+@section('content')
+
+<h2>History</h2>
+
+<div class="history">
+
+<h3>Early</h3>
+<p>I first got a computer aged 10 (a BBC Micro 32k) and while it came with loads of games, it also came with a book - 32 Hour Basic. Here I made early forays into programming simple games and graphics demos learning about logic, flow control, memory and abstraction</p>
+<p>At university I learned the all powerful C as well as embedded systems and signal processing. I learned about memory management, pointer arithmetic, code organisation and interfacing with hardware at a low level as well as some assembly and how modern machines actually work</p>
+<p>I went on to try out many different languages and systems from a CD-rom I sent off for from the University of Brighton. I tried my hand at Java, Ada, VRML and many more.</p>
+
+<h3>Later</h3>
+<p>At the turn of the millennium I wanted to build a music streaming/distribution system that would catalogue/index an MP3 collection and deliver it to a web client with automatic beat matched mixing of tracks. I started by building CGI gateways in C, backed by a PostGreSQL database</p>
+<p>While this project got quite far and served as a great introduction to Linux and web applications it was never finished, the advent of Napster and music clients with these functions built in and the beginning of digital music, my efforts were quickly superseded</p>
+<p>With my first few jobs I found ways to use my increasing technical knowledge to improve, speed up and automate processes moving on to bigger and longer term projects, around this time I found PHP and started learning more about web applications</p>
+<p>I pushed my knowledge further in this area by building my own content management system in order to quickly and easily build websites for people and businesses. This proved not only to be a great learning exercise but did in fact get used to produce several commercial sites and even helped me get my first job in web development!</p>
+
+<h3>Career</h3>
+<p>My first proper tech job was as an application developer for <a href="https://www.airangel.com">Air Angel</a> - I learned a great deal about building and running commercial web applications, linux servers, service integration and my first exposure to the cloud</p>
+<p>I moved on to work as a developer at <a href="https://www.6dg.co.uk">Six Degrees Group</a>, an integrated service provider with their own datacentres. Here I learned lots about how large scale internet infrastructure is built, managed and scaled. From hardware to billing and integration of all kinds of services and data, this was a great source of information about all kinds of technology</p>
+<p>My next move was to a mobile web focused agency called Wapple - they specialised in creating great web experiences on all kinds of mobile devices and had done from the early 2000s. This was around the time that people had m.domain.com type mobile sites and soon started moving toward responsive design. I learned about all kinds of front end technology and how to build larger scale applications with one reaching 500k users in a few months. I started to build and maintain large scale application architecture and learned how to migrate large apps with little or no downtime as well as how to optimise demanding workloads and manage cloud resources and budgets</p>
+
+<h3>Current</h3>
+<p>Today I work at <a href="https://crworldwide.com">CR Worldwide</a> as Technical Architect. I am responsible for planning, building and maintaining our cloud infrastructure and ensuring our platforms and bespoke applications are healthy, secure and performing well. I work on new and existing products with our in-house teams as well as a few external agencies to develop great solutions for our clients as well as our own tools and services. It's a fast paced role with lots to learn and I really enjoy helping others to learn new skills, bring products to life and solve problems.</p>
+<p>While we mostly use Ruby for our applications we also have some in PHP in recently we have been developing a lot of business logic in Python using Lambdas, a very useful serverless technology from AWS.</p>
+<p>I regularly develop ideas for products and services in my own time too and often tinker with hardware and software projects at home</p>
+
+</div>
+
+@endsection
